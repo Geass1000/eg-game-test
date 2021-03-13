efa722a52c263235d9330bce271ea8576158aff3
Andrey <geass1000@gmail.com>
2021-03-13 13:17:52 +0300

    feat(app): add base implementation of `Hexagon` component

--- src/app/hexagon/hexagon.component.ts
+++ src/app/hexagon/hexagon.component.ts
@@ -1,20 +1,110 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  OnInit,
+} from '@angular/core';
+
+const SmallRadiusParam = Math.sqrt(3) / 2;
+const DefaultRadius = 100;
+const DefaultStroke = 5;
 
 @Component({
   selector: 'eg-hexagon',
   templateUrl: './hexagon.component.svg',
   styleUrls: [ './hexagon.component.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HexagonComponent implements OnInit {
+  /**
+   * Path points of `path` tag with a hexagon.
+   */
+  public path: string;
+  /**
+   * SVG view box.
+   */
+  public viewBox: string;
+  /**
+   * Width of SVG tag.
+   */
+  public width: number;
+  /**
+   * Height of SVG tag.
+   */
+  public height: number;
 
-  public radius: number;
+  /**
+   * Radius (r) of inscribed circle.
+   *
+   * https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Regular_hexagon_1.svg/200px-Regular_hexagon_1.svg.png
+   */
+  public smallRadius: number;
+  /**
+   * Radius (R) of circumscribed circle.
+   *
+   * https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Regular_hexagon_1.svg/200px-Regular_hexagon_1.svg.png
+   */
+  public radius: number = DefaultRadius;
   @Input('raius')
   set inRadius (value: number) {
+    this.radius = _.isNaN(+value) === true
+      ? DefaultRadius : value;
+    this.updateView();
   }
 
-  constructor () { }
+  /**
+   * Width of hexagon stroke.
+   */
+  public strokeWidth: number = DefaultStroke;
+  @Input('strokeWidth')
+  set isStrokeWidth (value: number) {
+    this.strokeWidth = _.isNaN(+value) === true
+      ? DefaultStroke : value;
+    this.updateView();
+  }
 
+  constructor (
+    private changeDetection: ChangeDetectorRef,
+  ) {
+    this.changeDetection.detach();
+  }
+
+  /**
+   * Inits component.
+   */
   ngOnInit (): void {
+    this.updateView();
   }
 
+  /**
+   * Recalculates:
+   *  - width and height of SVG tag
+   *  - view box of SVG tag
+   *  - hexagon path
+   *
+   * @return {void}
+   */
+  updateView (
+  ): void {
+    this.smallRadius = this.radius * SmallRadiusParam;
+
+    this.width = this.radius * 2 + this.strokeWidth;
+    this.height = this.smallRadius * 2 + this.strokeWidth;
+
+    this.viewBox = `0 0 ${this.width} ${this.height}`;
+
+    const pathParts = [
+      `M${this.radius / 2 + this.strokeWidth / 2} ${0 + this.strokeWidth / 2}`, // Top-Left point
+      `l${this.radius} ${0}`, // Top-Right point
+      `l${this.radius / 2} ${this.smallRadius}`, // Right point
+      `l${-this.radius / 2} ${this.smallRadius}`, // Bottom-Right point
+      `l${-this.radius} ${0}`, // Bottom-Left point
+      `l${-this.radius / 2} ${- this.smallRadius}`, // Left point
+      `Z`, // Top-Left point
+    ];
+    this.path = pathParts.join(` `);
+
+    this.changeDetection.detectChanges();
+  }
 }
