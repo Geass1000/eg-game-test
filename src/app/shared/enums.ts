575f5a168322c618a25ed36385d7b91d31e2d1fe
Andrey <geass1000@gmail.com>
2021-03-13 23:03:10 +0300

    feat(app): add `Game Status` and  `Hexagon Coords Type` enums

---
+++ src/app/shared/enums.ts
@@ -0,0 +1,12 @@
+
+export enum GameStatus {
+  Settings = 'settings',
+  Playing = 'playing',
+  GameOver = 'game-over',
+}
+
+export enum HexagonCoordsType {
+  Cube = 'cube',
+  Axial = 'axial',
+  Offset = 'offset',
+}
