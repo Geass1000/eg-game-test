d7624bf6f4b06d880fc22826789ae7a4221f2448
Andrey <geass1000@gmail.com>
2021-03-14 13:47:50 +0300

    feat(services): add `createHexagonManagerFromHexagon` logic to `Engine` factory

--- src/app/services/engine.factory.ts
+++ src/app/services/engine.factory.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import * as Managers from '../managers';
-import { Interfaces } from '../shared';
+import { Interfaces, Enums } from '../shared';
 
 import { HexagonCoordsConverterService } from './hexagon-coords-converter.service';
 
@@ -18,16 +18,40 @@ export class EngineFactory {
    * Creats an instance of `Hexagon` manager.
    *
    * @param  {Interfaces.HexagonCoords} coords
+   * @param  {THexagonValue} [value]
    * @return {Managers.HexagonManager}
    */
-  createHexagonManager (
+  createHexagonManager <THexagonValue = any> (
     coords: Interfaces.HexagonCoords,
-  ): Managers.HexagonManager {
-    const hexagonManager = new Managers.HexagonManager(
+    value?: THexagonValue,
+  ): Managers.HexagonManager<THexagonValue> {
+    const hexagonManager = new Managers.HexagonManager<THexagonValue>(
       // Service
       this.hexagonCoordsConverterService,
     );
-    hexagonManager.$init(coords);
+    hexagonManager.$init(coords, value);
+    return hexagonManager;
+  }
+
+  /**
+   * Creats an instance of `Hexagon` manager from plain hexagon object.
+   *
+   * @param  {Interfaces.Hexagon<THexagonValue>} hexagon
+   * @return {Managers.HexagonManager<THexagonValue>}
+   */
+  createHexagonManagerFromHexagon <THexagonValue = any> (
+    hexagon: Interfaces.Hexagon<THexagonValue>,
+  ): Managers.HexagonManager<THexagonValue> {
+    const hexagonManager = new Managers.HexagonManager<THexagonValue>(
+      // Service
+      this.hexagonCoordsConverterService,
+    );
+    hexagonManager.$init({
+      type: Enums.HexagonCoordsType.Cube,
+      x: hexagon.x,
+      y: hexagon.y,
+      z: hexagon.z,
+    }, hexagon.value);
     return hexagonManager;
   }
 }
