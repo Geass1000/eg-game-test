fa4bf9770ab213b3708505c38200f711b6500fde
Andrey <geass1000@gmail.com>
2021-03-14 20:59:18 +0300

    chore(app): rename `App Routing` module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRouter } from './app.router';
 
 // Components
 import { AppComponent } from './app.component';
@@ -18,7 +18,7 @@ import { GameService } from './services/game.service';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRouter,
   ],
   declarations: [
     AppComponent,
