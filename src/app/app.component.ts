906ac99b333a5d29e3e7db8eaed2189bba3aaff5
Andrey <geass1000@gmail.com>
2021-03-16 03:08:36 +0300

    feat(app): add `URL Fragment` logic to `App` component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { GameItemsArbiter } from './services/game-items.arbiter';
+import { GameParamsArbiter } from './services/game-params.arbiter';
+
+import { BaseComponent } from './shared';
 
 @Component({
   selector: 'eg-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  title = 'eg-test-game';
+export class AppComponent extends BaseComponent implements OnInit {
+  public gridRadius: number;
+
+  constructor (
+    // Angular
+    protected changeDetection: ChangeDetectorRef,
+    private route: ActivatedRoute,
+    // Services
+    private gameItemsArbiter: GameItemsArbiter,
+    private gameParamsArbiter: GameParamsArbiter,
+  ) {
+    super(changeDetection);
+  }
+
+  ngOnInit (
+  ): void {
+    const routeURL$ = this.route.fragment.subscribe((fragment) => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      this.onURLFragment(fragment);
+    });
+    this.registrator.subscribe(routeURL$);
+    this.render();
+  }
+
+  /**
+   * Parses a url fragment. If it's a valid, fn will init a new game.
+   *
+   * @param  {string} fragment
+   * @return {Promise<void>}
+   */
+  async onURLFragment (
+    fragment: string,
+  ): Promise<void> {
+    const parsedFragment = /test(\d+)/.exec(fragment);
+
+    if (_.isNil(parsedFragment) === true) {
+      return;
+    }
+
+    const radius = +parsedFragment[1];
+
+    if (radius < 2) {
+      console.warn(`AppComponent`);
+      return;
+    }
+
+    this.gridRadius = radius;
+    this.gameParamsArbiter.gameGridRadius = this.gridRadius;
+    await this.initGame();
+  }
+
+  /**
+   * Inits a new game:
+   *  - inites the game items arbiter.
+   *
+   * @return {Promise<void>}
+   */
+  async initGame (
+  ): Promise<void> {
+    await this.gameItemsArbiter.$init();
+  }
 }
