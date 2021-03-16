f7985d2ff879f69c7b59e70cabac5586437f218d
Andrey <geass1000@gmail.com>
2021-03-16 03:06:03 +0300

    feat(app): add `HttpClient` module and change path to `Hexagon` component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRouter } from './app.router';
 
 // Components
 import { AppComponent } from './app.component';
-import { HexagonComponent } from './hexagon';
+import { HexagonComponent } from './components/hexagon';
 import { GameGridComponent } from './components/game-grid';
 import { GameAreaComponent } from './components/game-area';
 import { GameItemsComponent } from './components/game-items';
@@ -21,6 +22,7 @@ import { GameService } from './services/game.service';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRouter,
   ],
   declarations: [
