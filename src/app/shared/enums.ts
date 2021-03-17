a5fe41846e5472e74de0ec3d3563d0de1929c2d8
Andrey <geass1000@gmail.com>
2021-03-18 02:59:58 +0300

    feat(shared): move hexagon enums to separate file

--- src/app/shared/enums.ts
+++ src/app/shared/enums.ts
@@ -1,27 +1,8 @@
 
+export * from './enums/hexagon.enums';
+
 export enum GameStatus {
   Settings = 'settings',
   Playing = 'playing',
   GameOver = 'game-over',
-}
-
-export enum HexagonCoordsType {
-  Cube = 'cube',
-  Axial = 'axial',
-  Offset = 'offset',
-}
-
-export enum MoveDirection {
-  TopRight = 'TopRight',
-  TopLeft = 'TopLeft',
-  Top = 'Top',
-  BottomRight = 'BottomRight',
-  BottomLeft = 'BottomLeft',
-  Bottom = 'Bottom',
-}
-
-export enum Axis {
-  X = 'x',
-  Y = 'y',
-  Z = 'z',
 }
---
+++ src/app/shared/enums/hexagon.enums.ts
@@ -0,0 +1,21 @@
+
+export enum HexagonCoordsType {
+  Cube = 'cube',
+  Axial = 'axial',
+  Offset = 'offset',
+}
+
+export enum MoveDirection {
+  TopRight = 'TopRight',
+  TopLeft = 'TopLeft',
+  Top = 'Top',
+  BottomRight = 'BottomRight',
+  BottomLeft = 'BottomLeft',
+  Bottom = 'Bottom',
+}
+
+export enum Axis {
+  X = 'x',
+  Y = 'y',
+  Z = 'z',
+}
