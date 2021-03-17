c081bd6c32a563ef25f4b4a8b10718b75183b6a5
Andrey <geass1000@gmail.com>
2021-03-18 02:51:33 +0300

    fix(services): move merged hexagons to positive direction in `mergeHexagons` logic of `Game Items` arbiter

--- src/app/services/game-items.arbiter.ts
+++ src/app/services/game-items.arbiter.ts
@@ -130,6 +130,8 @@ export class GameItemsArbiter extends BaseService {
     const sortedHexagons = _.orderBy(hexagons, [ positiveAxis ], [ 'desc' ]);
 
     let maxPositiveHexagonCoords = this.getMaxPositiveHexagonCoords(mainAxisValue, moveDirection);
+    const inversedMoveDirection = this.inverseDirection(moveDirection);
+    const negativeDirectionOffset = this.getOffsetByDirection(inversedMoveDirection);
 
     let i = 0;
     const mergedHexagons: HexagonManager<number>[] = [];
@@ -137,26 +139,84 @@ export class GameItemsArbiter extends BaseService {
     while (i < sortedHexagons.length) {
       const toHexagon = sortedHexagons[i];
       const mergeHexagon = sortedHexagons[i + 1];
-      // If we can't merge ToHexagon with MergeHexagon we will add ToHexagon to result array
-      // and skip ToHexagon
+      let hexagonValue: number;
       if (this.canBeMerged(toHexagon, mergeHexagon) === false) {
-        mergedHexagons.push(toHexagon);
+        // If we can't merge ToHexagon with MergeHexagon we will add ToHexagon to result array
+        // and skip ToHexagon
+        hexagonValue = toHexagon.value;
         // Skip ToHexagon (1)
         i++;
-        continue;
+      } else {
+        // If we can merge ToHexagon with MergeHexagon we will add MergedHexagon to result array
+        // and skip ToHexagon and MergeHexagon
+        hexagonValue = toHexagon.value * 2;
+        // Skip ToHexagon and MergeHexagon (2)
+        i += 2;
       }
 
-      // If we can merge ToHexagon with MergeHexagon we will add MergedHexagon to result array
-      // and skip ToHexagon and MergeHexagon
-      const mergedHexagonCoords = toHexagon.getCoordsInCube();
-      const mergedHexagon = this.engineFactory
-        .createHexagonManager(mergedHexagonCoords, toHexagon.value * 2);
+      const toHexagonCoords = toHexagon.getCoordsInCube();
+      // FYI[Optimization]: We can create a link-direction array to skip neighboring items which
+      // weren't changed in a new iteration
+      const mergedHexagon = toHexagon.value === hexagonValue
+        && this.compareHexagonsCoords(toHexagonCoords, maxPositiveHexagonCoords) === true
+        ? toHexagon : this.engineFactory.createHexagonManager(maxPositiveHexagonCoords, hexagonValue);
       mergedHexagons.push(mergedHexagon);
-      // Skip ToHexagon and MergeHexagon (2)
-      i += 2;
+
+      // Move max positive coords to -1 position by the main axis
+      maxPositiveHexagonCoords = this.addCoords(maxPositiveHexagonCoords, negativeDirectionOffset);
     }
 
     return mergedHexagons;
+  }
+
+  /**
+   * Returns `true` if hexagons are equal.
+   *
+   * @param  {Interfaces.Hexagon} hexagon1
+   * @param  {Interfaces.Hexagon} hexagon2
+   * @return {boolean}
+   */
+  compareHexagons (
+    hexagon1: Interfaces.Hexagon,
+    hexagon2: Interfaces.Hexagon,
+  ): boolean {
+    return hexagon1.value === hexagon2.value
+      && this.compareHexagonsCoords(hexagon1, hexagon2);
+  }
+
+  /**
+   * Returns `true` if hexagons coords are equal.
+   *
+   * @param  {Interfaces.Hexagon} hexagon1
+   * @param  {Interfaces.Hexagon} hexagon2
+   * @return {boolean}
+   */
+  compareHexagonsCoords (
+    hexagon1: Interfaces.Hexagon,
+    hexagon2: Interfaces.Hexagon,
+  ): boolean {
+    return hexagon1.x === hexagon2.x
+      && hexagon1.y === hexagon2.y
+      && hexagon1.z === hexagon2.z;
+  }
+
+  /**
+   * Adds `Cube` coordinates 1 to `Cube` coordinates 2 and returns a result.
+   *
+   * @param  {Interfaces.HexagonCubeCoords} coords1
+   * @param  {Interfaces.HexagonCubeCoords} coords2
+   * @return {Interfaces.HexagonCubeCoords}
+   */
+  addCoords (
+    coords1: Interfaces.HexagonCubeCoords,
+    coords2: Interfaces.HexagonCubeCoords,
+  ): Interfaces.HexagonCubeCoords {
+    return {
+      type: Enums.HexagonCoordsType.Cube,
+      x: coords1.x + coords2.x,
+      y: coords1.y + coords2.y,
+      z: coords1.z + coords2.z,
+    };
   }
 
   /**
