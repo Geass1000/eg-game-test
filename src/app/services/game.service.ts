1655fdcf34140e9ba26850922da5fc3b1c70d188
Andrey <geass1000@gmail.com>
2021-03-19 02:12:20 +0300

    feat(services): don't use `Hexagon` manager in `Game` service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import * as Managers from '../managers';
 import { Interfaces } from '../shared';
 
 import { environment } from '../../environments/environment';
@@ -27,29 +26,19 @@ export class GameService {
    * @return {Promise<Managers.HexagonManager[]>}
    */
   async getNewHexagons (
-    nonEmptyHexagons: Managers.HexagonManager[],
-  ): Promise<Managers.HexagonManager[]> {
+    nonEmptyHexagons: Interfaces.Hexagon[],
+  ): Promise<Interfaces.Hexagon[]> {
     const gameRadius = this.gameParamsArbiter.gameGridRadius;
-
-    const preparedHexagons = _.map(nonEmptyHexagons, (nonEmptyHexagon) => {
-      const plainHexagon = nonEmptyHexagon.toJSON();
-      return plainHexagon;
-    });
 
     try {
       const newHexagons = await this.http.post<Interfaces.Hexagon[]>(
         `${environment.apiUrl}/${gameRadius}`,
-        preparedHexagons,
+        nonEmptyHexagons,
       ).toPromise();
       console.log(`GameService.getNewHexagons:`,
         `We have loaded next elements:`, newHexagons);
 
-      const newHexagonManagers = _.map(newHexagons, (newHexagon) => {
-        const newHexagonManager = this.engineFactory.createHexagonManagerFromHexagon(newHexagon);
-        return newHexagonManager;
-      });
-
-      return newHexagonManagers;
+      return newHexagons;
     } catch (error) {
       console.error(`GameService.getNewHexagons:`,
         `We can't load a data from the server. Error:`, error);
