00bea38ac7b55e483cbd8063e1449de62910d3a8
Andrey <geass1000@gmail.com>
2021-03-22 02:53:11 +0300

    feat(pages): add logic which won't start a new game without server url in `Game` page

--- src/app/pages/game-page/game.page.ts
+++ src/app/pages/game-page/game.page.ts
@@ -7,6 +7,12 @@ import {
 
 import { BaseComponent } from '../../shared';
 
+// Services
+import { GameItemsArbiter } from '../../services/game-items.arbiter';
+
+// State Store
+import { StateStore } from '../../state-store/state-store.service';
+
 @Component({
   selector: 'eg-game-page',
   templateUrl: './game.page.html',
@@ -14,9 +20,14 @@ import { BaseComponent } from '../../shared';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GamePageComponent extends BaseComponent implements OnInit {
+  public gameIsStarted: boolean;
 
   constructor (
     protected changeDetection: ChangeDetectorRef,
+    // Services
+    private gameItemsArbiter: GameItemsArbiter,
+    // State Store
+    private stateStore: StateStore,
   ) {
     super(changeDetection);
   }
@@ -24,18 +35,46 @@ export class GamePageComponent extends BaseComponent implements OnInit {
   /**
    * Inits component.
    */
-  ngOnInit (): void {
-    this.updateView();
-  }
+  async ngOnInit (): Promise<void> {
+    const ssDataServerURL$ = this.stateStore.select([ 'game', 'dataServerURL' ])
+      .subscribe(() => {
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        this.updateView();
+      });
+    this.registrator.subscribe(ssDataServerURL$);
 
-  /**
-   * Renders view.
-   *
-   * @return {void}
-   */
-  updateView (
-  ): void {
+    await this.updateView();
     this.render();
   }
 
+  /**
+   * Updates game server and restarts game.
+   *
+   * @return {Promise<void>}
+   */
+  async updateView (
+  ): Promise<void> {
+    this.gameIsStarted = this.shouldStartGame() === true;
+    if (this.gameIsStarted === false) {
+      return;
+    }
+
+    await this.gameItemsArbiter.$init();
+    this.render();
+  }
+
+  /**
+   * Returns `true` if we can start a new game.
+   *
+   * @return {void}
+   */
+  shouldStartGame (
+  ): boolean {
+    const dataServerURL = this.stateStore.getState([ 'game', 'dataServerURL' ]);
+    if (_.isEmpty(dataServerURL) === true) {
+      return false;
+    }
+
+    return true;
+  }
 }
