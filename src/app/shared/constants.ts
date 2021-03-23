f0516df7c683861d1bec1fcea78712ac50f4a14e
Andrey <geass1000@gmail.com>
2021-03-23 03:06:22 +0300

    feat(shared): add `Server Options` constant

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,13 @@
+
+export const ServerOptions = [
+  {
+    id: `remote`,
+    value: `//68f02c80-3bed-4e10-a747-4ff774ae905a.pub.instances.scw.cloud`,
+    hint: `Remote server`,
+  },
+  {
+    id: `localhost`,
+    value: `http://localhost:13337/`,
+    hint: `Local server`,
+  },
+];
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,9 +1,11 @@
 import * as Interfaces from '../shared/interfaces';
 import * as Enums from '../shared/enums';
+import * as Constants from '../shared/constants';
 
 export {
   Interfaces,
   Enums,
+  Constants,
 };
 
 export { BaseComponent } from './base.component';
